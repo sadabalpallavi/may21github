25b896cb8f65e074c66caad85c01046201d189cb
sadabalpallavi <114722503+sadabalpallavi@users.noreply.github.com>
2022-10-07 18:11:13 -0800

    Update FirstClass.java
    Co-Authored-By: Mayurigunjal <114721569+Mayurigunjal@users.noreply.github.com>

--- 7octoberProject/src/TestBase/FirstClass.java
+++ 7octoberProject/src/TestBase/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("7 october");
+System.out.println("7 octo completed");
 	}
+	
 
 }
